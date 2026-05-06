a38022edb05793d06a5eb45e5152ee96cfd5068d
Sushant Kumar <124432314+sjha34471234@users.noreply.github.com>
2026-05-06 15:56:40 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'images.printify.com',
+      'storage.googleapis.com',
+      'via.placeholder.com'
+    ],
+  },
+}
+
+module.exports = nextConfig
