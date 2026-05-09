7299e9461836cbb6dd7734146c5ff970e69a6caa
Sushant Kumar <124432314+sjha34471234@users.noreply.github.com>
2026-05-09 17:28:25 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { useRouter } from 'next/navigation'
+import { Search, X, ShoppingCart, Menu, User } from 'lucide-react'
 import Link from 'next/link'
 import { useState } from 'react'
 import { ShoppingCart, Menu, X, User } from 'lucide-react'
